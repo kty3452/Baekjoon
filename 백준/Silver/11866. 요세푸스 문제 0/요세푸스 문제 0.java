b5de8d71f181b54e851eed91638c00fbd20cfa49
Tea-Yeon-Kim <2376573@gachon.ac.kr>
2025-05-03 18:33:24 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 216 ms, Memory: 18064 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -3,25 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), k = sc.nextInt(), arr[] = new int[n],  arr2[] = new int[n], temp = 0;
-        for(int i = 0;i < n;i++){
-            int cnt = 0;
-            while(true){
-                if(temp >= n)
-                    temp %= n;
-                if(arr[temp] == 0)
-                    cnt++;
-                if(cnt == k)
-                    break;
-                temp++;
-            }
-            arr[temp] = 1;
-            arr2[i] = temp + 1;
-        }
+        int n = sc.nextInt(), k = sc.nextInt();
+
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= n; i++) list.add(i);
+
         System.out.print("<");
-        for(int i = 0;i < arr2.length-1;i++){
-            System.out.print(arr2[i]+", ");
+        int index = 0;
+        while (!list.isEmpty()) {
+            index = (index + k - 1) % list.size();
+            System.out.print(list.remove(index));
+            if (!list.isEmpty()) System.out.print(", ");
         }
-        System.out.print(arr2[arr.length - 1]+">");
+        System.out.println(">");
     }
 }
