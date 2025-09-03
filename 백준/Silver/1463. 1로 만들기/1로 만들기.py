bef77110839de130505d3b996ea1b5204b79619d
TaeYeon-Kim <2376573@gachon.ac.kr>
2025-09-03 18:35:46 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 118552 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -6,15 +6,14 @@ def input():
     return sys.stdin.readline().strip()
 
 X = int(input())
-arr = [float("inf")] * (X + 1)
-arr[1] = 0
+arr = [i for i in range(1000001)]
 
-for i in range(1, X):
+for i in range(1, 1000000):
     if arr[i + 1] > arr[i] + 1:
         arr[i + 1] = arr[i] + 1
-    if i * 2 <= X and arr[i * 2] > arr[i] + 1:
+    if i * 2 <= 1000000 and arr[i * 2] > arr[i] + 1:
         arr[i * 2] = arr[i] + 1
-    if i * 3 <= X and arr[i * 3] > arr[i] + 1:
+    if i * 3 <= 1000000 and arr[i * 3] > arr[i] + 1:
         arr[i * 3] = arr[i] + 1
-print(arr[X])
+print(arr[X] - 1)
  
