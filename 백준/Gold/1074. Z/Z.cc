31509d28dbd4a1d8bd2d5b4813d63452c491eec8
TaeYeon-Kim <2376573@gachon.ac.kr>
2025-10-14 01:03:17 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.cc
+++ 백준/Gold/1074. Z/Z.cc
@@ -1,19 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int z(int n, int r, int c) {
-    if (n == 0) return 0;  // 1x1이면 방문 순서 = 0
-    int half = 1 << (n - 1);   // 블록 한 변의 절반
-    int area = half * half;    // 블록 하나의 면적 (n-1 단계 블록 크기)
-
-    // 4분면에 따라 offset 다름
-    if (r < half && c < half)            // 1사분면 (왼위)
-        return z(n - 1, r, c);
-    if (r < half && c >= half)           // 2사분면 (오위)
-        return area + z(n - 1, r, c - half);
-    if (r >= half && c < half)           // 3사분면 (왼아래)
-        return 2 * area + z(n - 1, r - half, c);
-    return 3 * area + z(n - 1, r - half, c - half);  // 4사분면 (오아래)
+// 비트 교차 (r, c → morton code)
+int morton_order(int r, int c) {
+    int res = 0;
+    for (int i = 0; i < 15; i++) { // n ≤ 15 (2^15 = 32768)
+        res |= ((r >> i) & 1) << (2 * i + 1); // r의 i번째 비트를 2*i+1위치에
+        res |= ((c >> i) & 1) << (2 * i);     // c의 i번째 비트를 2*i위치에
+    }
+    return res;
 }
 
 int main() {
@@ -22,5 +17,5 @@ int main() {
 
     int n, r, c;
     cin >> n >> r >> c;
-    cout << z(n, r, c);
+    cout << morton_order(r, c);
 }
