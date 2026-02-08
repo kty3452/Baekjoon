d8bd237e162b50d79a797958108c142ee5f43cbc
TaeYeon-Kim <2376573@gachon.ac.kr>
2026-02-08 17:26:20 +0900

    [Silver II] Title: 소가 길을 건너간 이유 5, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
+++ 백준/Silver/14465. 소가 길을 건너간 이유 5/소가 길을 건너간 이유 5.cc
@@ -13,18 +13,16 @@ int main(){
         cin >> idx;
         v[idx] = -1;
     } // 0은 신호등 존재, -1은 신호등 부재.
-    queue<int> sliding_win;
+    
     int now = 0;
     for(int i = 1;i <= k;i++){
-        sliding_win.push(v[i]);
         if(v[i] == -1) now++;
     }
+
     int best = now;
     for(int i = k + 1;i <= n;i++){
-        if(sliding_win.front() == -1) now--;
-        sliding_win.pop();
+        if(v[i - k] == -1) now--;
         if(v[i] == -1) now++;
-        sliding_win.push(v[i]);
         if(now < best) best = now;
     }
     cout << best;
